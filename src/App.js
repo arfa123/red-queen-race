abb41a058a39e9bbdf1f8d91faf7b74d3e8d7476
arfa123 <aphahmed@yahoo.com>
2020-07-18 15:01:15 +0500

    red queen race animated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import useWebAnimations from "@wellyshen/use-web-animations";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	useEffect(() => {
+		document.addEventListener("click", goFaster);
+
+		return () => {
+			document.removeEventListener("click", goFaster);
+		};
+	});
+
+	const spriteFrames = [
+		{ transform: "translateY(0)" },
+		{ transform: "translateY(-100%)" }
+	];
+
+	const { ref, getAnimation } = useWebAnimations({
+		keyframes: spriteFrames,
+		timing: {
+			duration: 600,
+			direction: "reverse",
+			fill: "forwards",
+			iterations: Infinity,
+			easing: "steps(7, end)"
+		},
+	});
+
+	const goFaster = () => {
+		getAnimation().updatePlaybackRate(getAnimation().playbackRate * 1.1);
+	};
+
+	return (
+		<div className="wrapper">
+			<div className="sky"></div>
+			<div className="earth">
+				<div id="red-queen_and_alice">
+					<img ref={ref}
+						src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
+						srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
+						alt="Alice and the Red Queen running to stay in place." />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
